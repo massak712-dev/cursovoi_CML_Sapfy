806e53b8ea7459838a157e3a5bf29257c73e1e75
sapfear <massak712@gmail.com>
2026-05-06 17:37:58 +0300

    первый коммит: интернет-магазин Sapfy

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const burger = document.querySelector('.burger');
+    const navMenu = document.querySelector('.nav-menu');
+    
+    if (burger && navMenu) {
+        burger.addEventListener('click', function() {
+            navMenu.classList.toggle('active');
+        });
+    }
+});
